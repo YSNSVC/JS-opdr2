7e15d73561e02e917072a27a8191a70cd494f0d9
YSNSVC <89729328+YSNSVC@users.noreply.github.com>
2021-11-07 20:45:33 +0100

    Add files via upload

---
+++ opdr2.js
@@ -0,0 +1,27 @@
+
+
+const age = 22
+const isFemale = true;
+const driverstatus = true;
+
+
+if (age >= 18) {
+
+    document.write("Your allowed");
+
+} else if (age < 18) {
+
+    document.write("Your to young, kindly requested to stay outside");
+}
+
+if (isFemale = true) {
+
+    document.write("Lady");
+
+}
+
+
+if (driverstatus = true) {
+
+    document.write("BOB")
+}
